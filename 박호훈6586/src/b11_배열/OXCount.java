283f5724d65d77ea6db40161d5ba586371d945dc
= <=>
2021-09-06 21:48:50 +0900

    2021-09-06 commit

---
+++ 박호훈6586/src/b11_배열/OXCount.java
@@ -0,0 +1,43 @@
+package b11_¹è¿­;
+
+public class OXCount {
+	
+	public static void main(String[] args) {
+		String[][][] quiz = new String[3][2][3];
+		
+		for(int i = 0; i < quiz.length; i++) {
+			for(int j = 0; j < quiz[i].length; j++) {
+				for(int k = 0; k < quiz[i][j].length; k++) {
+					quiz[i][j][k] = (i+j+k)%2 == 0 ? "O" : "X";
+				}	
+			}
+		}
+		
+		
+		for(int i = 0; i < quiz.length; i++) {
+			for(int j = 0; j < quiz[i].length; j++) {
+				for(int k = 0; k < quiz[i][j].length; k++) {
+					quiz[i][j][k] = (i+j+k)%2 == 0 ? "O" : "X";
+					System.out.print(quiz[i][j][k]);
+				}
+				
+				System.out.print(quiz[i].length - 1 == j ? "\n" : ", ");
+				
+				
+				
+					/**				
+					if(quiz[i].length - 1 == j) {
+						System.out.println();
+					} else {
+					System.out.print(", ");
+					}
+					*/
+			}
+			
+		}
+		
+	}
+	
+	
+
+}
